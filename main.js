a0b18eb339b15cb6a88beb877dac1d940bafcdad
aroe <lipton.happy.milktea@gmail.com>
2019-10-26 17:35:00 +0900

    issue #3

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ window.addEventListener("devicemotion",
             oldSpeed = speed[i];
 
         }
-        $('#speed').text(`speed = ${speed}`);
+        $('#speed').text(`speed\n${speed[0].toFixed(2)}\n${speed[1].toFixed(2)}\n${speed[2].toFixed(2)}`);
 
         // var rotz = event.rotationRate.alpha; //z方向
         // var rotx = event.rotationRate.beta; //x方向
