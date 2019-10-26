67dbe68b1b5499e7f115766d9afa896338bea175
aroe <lipton.happy.milktea@gmail.com>
2019-10-26 13:43:37 +0900

    issue #3

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-document.getElementById("request_permission").addEventListener("click", function(){
-    if (
-      DeviceMotionEvent &&
-      DeviceMotionEvent.requestPermission &&
-      typeof DeviceMotionEvent.requestPermission === 'function'
-    ) {
-      DeviceMotionEvent.requestPermission();
-    }
-    if (
-      DeviceOrientationEvent &&
-      DeviceOrientationEvent.requestPermission &&
-      typeof DeviceOrientationEvent.requestPermission === 'function'
-    ) {
-      DeviceOrientationEvent.requestPermission();
-    }
-  })
+if (
+    DeviceMotionEvent &&
+    DeviceMotionEvent.requestPermission &&
+    typeof DeviceMotionEvent.requestPermission === 'function'
+) {
+    DeviceMotionEvent.requestPermission();
+}
+if (
+    DeviceOrientationEvent &&
+    DeviceOrientationEvent.requestPermission &&
+    typeof DeviceOrientationEvent.requestPermission === 'function'
+) {
+    DeviceOrientationEvent.requestPermission();
+}
