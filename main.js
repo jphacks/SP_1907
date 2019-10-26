6755717d3b85dc2d4f9ab6aba4ab5aad9ad7d8dd
aroe <lipton.happy.milktea@gmail.com>
2019-10-26 14:04:05 +0900

    issue #3

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-if (
-    DeviceMotionEvent &&
-    DeviceMotionEvent.requestPermission &&
-    typeof DeviceMotionEvent.requestPermission === 'function'
-) {
-    DeviceMotionEvent.requestPermission();
-}
-if (
-    DeviceOrientationEvent &&
-    DeviceOrientationEvent.requestPermission &&
-    typeof DeviceOrientationEvent.requestPermission === 'function'
-) {
-    DeviceOrientationEvent.requestPermission();
-}
+document.getElementById("request_permission").addEventListener("click", function(){
+    if (
+      DeviceMotionEvent &&
+      DeviceMotionEvent.requestPermission &&
+      typeof DeviceMotionEvent.requestPermission === 'function'
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
+    if (
+      DeviceOrientationEvent &&
+      DeviceOrientationEvent.requestPermission &&
+      typeof DeviceOrientationEvent.requestPermission === 'function'
+    ) {
+      DeviceOrientationEvent.requestPermission();
+    }
+  })
