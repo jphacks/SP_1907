647fe662a30fc45690c38647f39dfb36761be25e
aroe <lipton.happy.milktea@gmail.com>
2019-10-27 07:41:03 +0900

    issue#6

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ request_permission = function () {
         DeviceOrientationEvent.requestPermission();
     }
     // $("#container").html(innerHtml);
-    $("a-scene").html(innerHtml);
+    $("body").html(innerHtml);
 
 }
 
